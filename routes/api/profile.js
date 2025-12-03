7c061b53491dbbd2a44f1878520ef7d7d3c28599
Jonathan Grassis <jonathan.grassis@pega.com>
2025-12-03 09:45:29 -0500

    Create, update, get, and delete profile data

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,169 @@
 const router = require('express').Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const { check, validationResult } = require('express-validator');
 
+// @route   GET api/profile/me
+// @desc    Get current user's profile
+// @access  Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+    if (!profile) {
+      return res.status(400).json({ msg: "There's no profile for this user" });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.sendStatus(500);
+  }
+});
 
-// @route   GET api/profile
-// @desc    test Profile route
+// @route   POST api/profile/
+// @desc    Create or update a user profile
+// @access  Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is required').not().isEmpty(),
+      check('skills', 'Skills is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+
+    // Check fields sent in req body
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+
+    // Setup new object for Profile Fields, and associate the User ID with it
+    const profileFields = {};
+    profileFields.user = req.user.id;
+
+    // Build profile object
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(',').map((skill) => skill.trim());
+    }
+
+    // Build socials object
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (instagram) profileFields.social.instagram = instagram;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+
+    //
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+      if (profile) {
+        // Update an existing Profile
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        return res.json(profile);
+      } else {
+        // Create a new Profile
+        profile = new Profile(profileFields);
+        await profile.save();
+        res.json(profile);
+      }
+    } catch (err) {
+      console.error(err.message);
+      res.sendStatus(500);
+    }
+  }
+);
+
+// @route   GET api/profile/
+// @desc    Get all profiles
 // @access  Public
-router.get('/', (req,res) =>  res.send('Profile route'));
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.sendStatus(500);
+  }
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get Profile by User Id
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate('user', ['name', 'avatar']);
+    if (!profile) {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err);
+    if (err.kind === 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.sendStatus(500);
+  }
+});
+
+// @route   DELETE api/profile/user/:user_id
+// @desc    Delete Profile, User, and Posts by User Id
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo Remove User's posts
+
+    // Remove Profile
+    await Profile.findOneAndDelete({
+      user: req.user.id,
+    });
+
+    // Remove User
+    await User.findOneAndDelete({
+      _id: req.user.id,
+    });
+
+    res.json({ msg: `User ${req.user.id} Deleted` });
+  } catch (err) {
+    console.error(err);
+    if (err.kind === 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.sendStatus(500);
+  }
+});
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,87 +2,94 @@ const router = require('express').Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const {check, validationResult} = require('express-validator');
+const { check, validationResult } = require('express-validator');
 const User = require('../../models/User');
-const config = require ('config');
+const config = require('config');
 
 // @route   POST api/users
 // @desc    Register User
 // @access  Public
-router.post('/', [
+router.post(
+  '/',
+  [
     // Configure validation rules for a request body
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Please enter a password with 6 or more characters').isLength({min:6}),
-], async (req,res) =>  {
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
     // Pass the request to the validationResult() method to check payload against requirements
     const errors = validationResult(req);
 
     // If there are errors, return 400 and send the errors back
-    if(!errors.isEmpty()){
-        return res.status(400).json({ errors : errors.array() })
-    };
-    
-    // Destructure values from the req body
-    const {name, email, password} = req.body;
-
-    try {
-        // Make sure user isn't arleady registered
-        let user = await User.findOne({email});
-        
-        // If already registered, throw a 400 and return error
-        if(user){
-            return res.status(400).json({ errors : [ { msg : 'User already exists'} ] })
-        }
-
-        // Request avatar if exists, default if not
-        const avatar = gravatar.url(email, {
-            s: "200",
-            r: "pg",
-            default: "mm",
-        });
-
-        // Create new user from User model
-        user = new User({
-            name,
-            email,
-            avatar,
-            password:null,
-        });
-
-        // Create encryption salt
-        const salt = await bcrypt.genSalt(10);
-
-        // Encrypt the password for security
-        user.password = await bcrypt.hash(password, salt);
-
-        // Save the user to the DB
-        await user.save();
-
-        const jwtPayload = {
-            user: {
-                id: user.id,
-            },
-        };
-
-        jwt.sign(
-            jwtPayload, 
-            config.get('jwtSecret'),
-            { expiresIn: 360000 },
-            (err, token) => {
-                if(err) throw err
-                res.json({ token });
-            },
-        );
-
-    }catch(err){
-        // If there's an error, log it
-        console.error(err.message);
-
-        // Set the status to 500 and send an error message back
-        res.sendStatus(500);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-});
+    // Destructure values from the req body
+    const { name, email, password } = req.body;
+
+    try {
+      // Make sure user isn't arleady registered
+      let user = await User.findOne({ email });
+
+      // If already registered, throw a 400 and return error
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      // Request avatar if exists, default if not
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        default: 'mm',
+      });
+
+      // Create new user from User model
+      user = new User({
+        name,
+        email,
+        avatar,
+        password: null,
+      });
+
+      // Create encryption salt
+      const salt = await bcrypt.genSalt(10);
+
+      // Encrypt the password for security
+      user.password = await bcrypt.hash(password, salt);
+
+      // Save the user to the DB
+      await user.save();
+
+      const jwtPayload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        jwtPayload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      // If there's an error, log it
+      console.error(err.message);
+
+      // Set the status to 500 and send an error message back
+      res.sendStatus(500);
+    }
+  }
+);
 
 module.exports = router;
