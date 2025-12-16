08b1b15a3b529c461d31ae23cca328c3686c76db
Jonathan Grassis <jonathan.grassis@pega.com>
2025-12-15 22:35:10 -0500

    pushing progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
+// Framework Imports
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+// State Imports
+import store from './store';
+
+// Component Imports
 import NavBar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import PageSection from './components/layout/PageSection';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+
+// Other
 import './App.css';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <NavBar />
-      <Routes>
-        <Route exact path='/' element={<Landing />} />
-        <Route element={<PageSection />}>
-          <Route exact path='/register' element={<Register />} />
-          <Route exact path='/login' element={<Login />} />
-        </Route>
-      </Routes>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <NavBar />
+        <Routes>
+          <Route exact path='/' element={<Landing />} />
+          <Route element={<PageSection />}>
+            <Route exact path='/register' element={<Register />} />
+            <Route exact path='/login' element={<Login />} />
+          </Route>
+        </Routes>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
--- client/src/components/layout/PageSection.js
+++ client/src/components/layout/PageSection.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Alert from '../layout/Alert';
 
 const PageSection = () => {
   return (
     <section className='container'>
+      <Alert />
       <Outlet />
     </section>
   );
